5ed797972590771b438f50216ea2a6a0afb8a029
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-01-30 16:03:32 +0100

    add visitor and fit interfaces for editor

--- analyzer-api/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/analyzer/api/IAnalyzer.java
+++ analyzer-api/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/analyzer/api/IAnalyzer.java
@@ -1,8 +1,10 @@
 package edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api;
 
 import java.util.Collection;
+import java.util.UUID;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.core.model.LanguageFeature;
 import spoon.reflect.CtModel;
+import spoon.reflect.declaration.CtElement;
 
 /**
  * IAnalyzer
@@ -10,4 +12,14 @@ import spoon.reflect.CtModel;
 public interface IAnalyzer {
 
   Report analyze(Collection<LanguageFeature> languageFeature, CtModel simulatorAST);
+
+  String getDescription();
+  String getName();
+
+
+  public default UUID getIdentifier() {
+    return UUID.randomUUID();
+  }
+
+  boolean canAnalyze(CtElement element);
 }
