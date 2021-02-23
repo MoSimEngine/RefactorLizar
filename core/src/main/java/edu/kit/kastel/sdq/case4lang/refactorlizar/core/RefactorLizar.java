714740ae38f868774195ce763a96dc4e21934ea6
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-02-23 15:28:32 +0000

    update CI

--- core/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/core/RefactorLizar.java
+++ core/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/core/RefactorLizar.java
@@ -1,9 +1,15 @@
 package edu.kit.kastel.sdq.case4lang.refactorlizar.core;
 
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api.IAnalyzer;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
 
 public class RefactorLizar {
   public static void main(String[] args) {
-    IAnalyzer.getAllAnalyzer().forEach(System.out::println);
+    ModularLanguage lang = new ModularLanguage(new LanguageParser().parseLanguage(args[0]));
+    SimulatorModel model = new SimulatorModel(new LanguageParser().parseLanguage(args[1]));
+    IAnalyzer.getAllAnalyzer().forEach(v -> {v.init(lang, model); if(v.supportsFullAnalysis()) {
+      v.fullAnalysis();
+    }});
   }
 }
