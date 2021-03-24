873d90ec054d3f784348f29f655922fbbf0a50e2
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-03-24 13:01:29 +0000

    fix buildd

--- analyzer-api/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/analyzer/api/SearchLevels.java
+++ analyzer-api/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/analyzer/api/SearchLevels.java
@@ -3,23 +3,26 @@ package edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api;
 import java.util.Arrays;
 
 /**
- * This defines different search level for analyzer.
- * A finer search level results in finer grained results, but slower performance.
+ * This defines different search level for analyzer. A finer search level results in finer grained
+ * results, but slower performance.
  */
-//TODO: Add members to doc 
+// TODO: Add members to doc
 public enum SearchLevels {
-  
-  COMPONENT("component"),
-  PACKAGE("package"),
-  TYPE("type"),
-  TYPE_MEMBER("type_member");
-  
-  private String levelName;
-  SearchLevels(String levelName) {
-    this.levelName = levelName;
-  }
+    COMPONENT("component"),
+    PACKAGE("package"),
+    TYPE("type"),
+    TYPE_MEMBER("type_member");
 
-  public static SearchLevels of(String levelName) {
-      return Arrays.stream(SearchLevels.values()).filter(v -> v.levelName.equalsIgnoreCase(levelName)).findAny().orElse(null);
-  }
+    private String levelName;
+
+    SearchLevels(String levelName) {
+        this.levelName = levelName;
+    }
+
+    public static SearchLevels of(String levelName) {
+        return Arrays.stream(SearchLevels.values())
+                .filter(v -> v.levelName.equalsIgnoreCase(levelName))
+                .findAny()
+                .orElse(null);
+    }
 }
