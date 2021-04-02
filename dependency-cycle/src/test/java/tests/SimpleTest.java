f5d109c0fe589d1dea8fad911fd69c19dd7aa4a5
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-04-03 00:46:32 +0200

    remove unneeded testcase

--- dependency-cycle/src/test/java/tests/SimpleTest.java
+++ dependency-cycle/src/test/java/tests/SimpleTest.java
@@ -2,35 +2,16 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api.SearchLevels;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.dependencycycle.CycleVisitor;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.core.LanguageParser;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.core.SimulatorParser;
-import edu.kit.kastel.sdq.case4lang.refactorlizar.model.Feature;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
-import java.util.Collection;
-import java.util.stream.Collectors;
-import org.junit.jupiter.api.Test;
-import spoon.Launcher;
-import spoon.reflect.CtModel;
 
 public class SimpleTest {
-
-    @Test
-    public void simpleLanguageTest() {
-
-        Launcher simulatorLauncher = new Launcher();
-        simulatorLauncher.addInputResource("./src/test/resources/SimpleExample");
-        CtModel simulatorModel = simulatorLauncher.buildModel();
-        Collection<Feature> features =
-                simulatorModel.getAllPackages().stream()
-                        .map(v -> new Feature(v, null))
-                        .collect(Collectors.toList());
-        SimulatorModel model = new SimulatorModel(features);
-        CycleVisitor visitor = new CycleVisitor();
-        System.out.println(visitor.fullAnalysis(model).toString());
-    }
 
     @Test
     public void typeLevelReport() {
