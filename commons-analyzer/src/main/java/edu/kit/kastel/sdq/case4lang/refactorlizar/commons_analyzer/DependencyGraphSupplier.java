e1d345155d1a2a1eeb718619385246acdec1998b
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-04-12 17:27:06 +0000

    fix formatting

--- commons-analyzer/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/commons_analyzer/DependencyGraphSupplier.java
+++ commons-analyzer/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/commons_analyzer/DependencyGraphSupplier.java
@@ -1,17 +1,17 @@
 package edu.kit.kastel.sdq.case4lang.refactorlizar.commons_analyzer;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import com.google.common.flogger.FluentLogger;
 import com.google.common.graph.Graphs;
 import com.google.common.graph.MutableNetwork;
 import com.google.common.graph.NetworkBuilder;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.CtTypeMember;
 import spoon.reflect.reference.CtTypeReference;
@@ -99,7 +99,6 @@ public class DependencyGraphSupplier {
                 .flatMap(v -> v.stream())
                 .collect(Collectors.toList());
     }
-
     private Set<CtType<?>> getAllTypes(SimulatorModel model) {
         return model.getAllElements(CtType.class).stream().collect(Collectors.toSet());
     }
