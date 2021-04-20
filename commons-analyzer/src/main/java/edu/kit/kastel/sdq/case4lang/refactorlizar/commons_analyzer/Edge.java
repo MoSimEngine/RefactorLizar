68faae93f834eeca40e45bd7ac627022ae0237fe
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-04-20 21:57:21 +0000

    add graph generation for type, package and component

---
+++ commons-analyzer/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/commons_analyzer/Edge.java
@@ -0,0 +1,47 @@
+package edu.kit.kastel.sdq.case4lang.refactorlizar.commons_analyzer;
+
+import java.util.Objects;
+
+/** Edge */
+public class Edge<T, R> {
+
+    private final T source;
+    private final T target;
+    private final R cause;
+    /**
+     * @param source
+     * @param target
+     * @param cause
+     */
+    public Edge(T source, T target, R cause) {
+        this.source = source;
+        this.target = target;
+        this.cause = cause;
+    }
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cause, source, target);
+    }
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Edge)) return false;
+        Edge other = (Edge) obj;
+        return Objects.equals(cause, other.cause)
+                && Objects.equals(source, other.source)
+                && Objects.equals(target, other.target);
+    }
+
+    /** @return the cause */
+    public R getCause() {
+        return cause;
+    }
+}
