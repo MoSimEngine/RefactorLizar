b8728c25c1367ed85976c364063e74b5364f10a9
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-05-10 18:28:23 +0000

    change dependency cycle to new graph

---
+++ commons-analyzer/src/main/java/edu/kit/kastel/sdq/case4lang/refactorlizar/commons_analyzer/Relation.java
@@ -0,0 +1,61 @@
+package edu.kit.kastel.sdq.case4lang.refactorlizar.commons_analyzer;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class Relation<T, U> {
+
+    private T source;
+    private T target;
+    private Collection<U> causes;
+
+    public Relation(T source, T target, Collection<U> causes) {
+        this.source = source;
+        this.target = target;
+        this.causes = causes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Relation)) {
+            return false;
+        }
+        Relation<?, ?> relation = (Relation<?, ?>) o;
+        return Objects.equals(source, relation.source)
+                && Objects.equals(target, relation.target)
+                && Objects.equals(causes, relation.causes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, target, causes);
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                + " source='"
+                + getSource()
+                + "'"
+                + ", target='"
+                + getTarget()
+                + "'"
+                + ", causes='"
+                + getCauses()
+                + "'"
+                + "}";
+    }
+
+    public T getSource() {
+        return source;
+    }
+
+    public T getTarget() {
+        return target;
+    }
+
+    public Collection<U> getCauses() {
+        return causes;
+    }
+}
