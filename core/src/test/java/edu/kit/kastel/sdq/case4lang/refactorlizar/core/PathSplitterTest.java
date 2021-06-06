b39857883c7299943af90ee022c32fcbe5d8156c
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-06-06 17:20:19 +0200

    add sysout

--- core/src/test/java/edu/kit/kastel/sdq/case4lang/refactorlizar/core/PathSplitterTest.java
+++ core/src/test/java/edu/kit/kastel/sdq/case4lang/refactorlizar/core/PathSplitterTest.java
@@ -11,6 +11,8 @@ public class PathSplitterTest {
         Path simulatorPath = Path.of("src/test/resources/simulatorInLanguage/SimuA");
         Path languagePath = Path.of("src/test/resources/simulatorInLanguage");
         var splitter = new PathSplitter(simulatorPath, languagePath).simulatorInLanguage().split();
+        System.out.println(simulatorPath.toString());
+        System.out.println(languagePath.toString());
         splitter.getLanguagePaths().forEach(System.out::println);
         splitter.getSimulatorPaths().forEach(System.out::println);
         assertThat(splitter.getLanguagePaths()).hasSize(4);
